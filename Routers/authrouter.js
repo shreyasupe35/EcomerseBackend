28816502bbb4e5eac4152ec99ba763541e55247a
ShreyaSupe <shreyasupe510@gmail.com>
2024-03-22 15:59:40 +0530

    middleware added

--- Routers/authrouter.js
+++ Routers/authrouter.js
@@ -1,6 +1,6 @@
 const authcontroller = require ("../controllers/authcontroller.js")
-
+const authMW=require("../middleware/auth.mw.js")
 module.exports=(app)=>{
-    app.post("/ecom/api/v1/auth/signup",authcontroller.signup)
+    app.post("/ecom/api/v1/auth/signup",[authMW.verifySignupBody],authcontroller.signup)
 
 }
---
+++ middleware/auth.mw.js
@@ -0,0 +1,48 @@
+/**
+ * create a mw will check if the request body is proper and correct
+ */
+const user_model=require("../models/user.model.js")
+const verifySignupBody=async (req,res,next)=>{
+    try {
+        //ccheck for the name
+        // check for all filedds
+        //check if the user is present already or not
+
+        if(!req.body.name){
+            return res.status(400).send({
+                message:"Failed!name was not provided"
+            })
+        }
+        if(!req.body.email){
+            return res.status(400).send({
+                message:"Failed!email was not provided"
+            })
+        }
+        if(!req.body.userId){
+            return res.status(400).send({
+                message:"Failed!userId was not provided"
+            })
+        }
+        
+
+
+        const user =await user_model.findOne({userId:req.body.userId})
+        if(user){
+            return res.status(400).send({
+                message:"Failed!user already  prseneted"
+            })
+        }
+
+        next()
+
+    } catch (error) {
+        console.log("Error whilevalidating the request body")
+        res.status(500).send({
+            message:"error while validating the request body"
+        })
+    }
+}
+
+module.exports={
+    verifySignupBody:verifySignupBody
+}
