e93334a795a0fbe30c14244e4a9aca811da99d58
ShreyaSupe <shreyasupe510@gmail.com>
2024-03-22 16:48:33 +0530

    added signin

--- Routers/authrouter.js
+++ Routers/authrouter.js
@@ -2,5 +2,8 @@ const authcontroller = require ("../controllers/authcontroller.js")
 const authMW=require("../middleware/auth.mw.js")
 module.exports=(app)=>{
     app.post("/ecom/api/v1/auth/signup",[authMW.verifySignupBody],authcontroller.signup)
-
+    /**
+     * post for sigin
+     */
+    app.post("/ecom/api/v1/auth/signin",authcontroller.signin)
 }
--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,6 +1,9 @@
 /**
  * i need to write the controller logic to register a user
  */
+
+const secret=require("../config/auth.config.js")
+const jwt=require("jsonwebtoken")
 const bcrypt=require("bcryptjs")
 const user_model=require("../models/user.model")
 exports.signup=async(req,res)=>{
@@ -42,4 +45,38 @@ exports.signup=async(req,res)=>{
         })
      }
      //3.return the response back to the user
+}
+
+
+exports.signin=async (req,res)=>{
+   //check if user id is present
+   const user=await user_model.findOne({userId:req.body.userId});
+   if(user==null){
+      return res.status(400).send({
+         message:"User id is not valid user ID"
+      })
+   }
+
+   //password is correct
+   const isPasswordValid=bcrypt.compareSync(req.body.password,user.password)
+   if(!isPasswordValid){
+     return  res.status(401).send({
+         message:"Wrong password passed"
+      })
+   }
+
+   //using jwt we will create the acess token with a given ttl and return
+   const token=jwt.sign({id:user.userId},secret.secret,{
+      expiresIn:120
+   })
+
+   res.status(200).send({
+      name:user.name,
+      userId:user.userId,
+      email:user.email,
+      userType:user.userType,
+      accessToken:token
+   })
+
+
 }
